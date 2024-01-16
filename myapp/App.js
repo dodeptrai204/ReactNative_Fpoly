584d4caaffc1dfaaddd81689aa24fcefab16f4f1
unknown <nqk0912aaa@gmail.com>
2024-01-16 13:16:42 +0700

    demo

---
+++ myapp/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Lab3 from './src/assets/components/screnns/Lab3';
+
+
+
+
+const App = () => {
+  return <Lab3></Lab3>;
+};
+
+export default App;
+
+
+
+
+
+
